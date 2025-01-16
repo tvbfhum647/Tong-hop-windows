39846a87b54386e388104835066caece0c9c720d
Snhvn <snhvnnguyen@gmail.com>
2025-01-16 19:11:27 +0700

    Create c.sh

---
+++ c.sh
@@ -0,0 +1,18 @@
+sudo cpulimit -l 80 -- sudo kvm \
+    -cpu host,+topoext,hv_relaxed,hv_spinlocks=0x1fff,hv-passthrough,+pae,+nx,kvm=on,+svm \
+    -smp 4,cores=4 \
+    -M q35,usb=on \
+    -device usb-tablet \
+    -m 12G \
+    -device virtio-balloon-pci \
+    -vga virtio \
+    -net nic,netdev=n0,model=virtio-net-pci \
+    -netdev user,id=n0,hostfwd=tcp::3389-:3389 \
+    -boot c \
+    -device virtio-serial-pci \
+    -device virtio-rng-pci \
+    -enable-kvm \
+    -hda /mnt/Win11.vhd \
+    -drive if=pflash,format=raw,readonly=off,file=/usr/share/ovmf/OVMF.fd \
+    -uuid e47ddb84-fb4d-46f9-b531-14bb15156336 \
+    -vnc :0

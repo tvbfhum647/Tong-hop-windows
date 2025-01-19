945b915223420115f8e1577df77ba27e06404a23
Snhvn <snhvnnguyen@gmail.com>
2025-01-19 07:49:13 +0700

    Create spice.sh

---
+++ spice.sh
@@ -0,0 +1,19 @@
+sudo cpulimit -l 80 -- sudo kvm \
+    -cpu host,+topoext,hv_relaxed,hv_spinlocks=0x1fff,hv-passthrough,+pae,+nx,kvm=on,+svm \
+    -smp 2,cores=2 \
+    -M q35,usb=on \
+    -device usb-tablet \
+    -m 8G \
+    -device virtio-balloon-pci \
+    -vga virtio \
+    -net nic,netdev=n0,model=virtio-net-pci \
+    -netdev user,id=n0,hostfwd=tcp::3389-:3389 \
+    -boot c \
+    -device virtio-serial-pci \
+    -device virtio-rng-pci \
+    -enable-kvm \
+    -hda /mnt/a.qcow2 \
+    -drive if=pflash,format=raw,readonly=off,file=/usr/share/ovmf/OVMF.fd \
+    -uuid e47ddb84-fb4d-46f9-b531-14bb15156336 \
+    -spice port=5924,disable-ticketing \
+    -vnc :0   

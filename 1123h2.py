cdbc9d412171cfe4dba4c62898eeb0dd6db7053f
Snhvn <snhvnnguyen@gmail.com>
2025-01-20 18:50:27 +0700

    Update 1123h2.py

--- 1123h2.py
+++ 1123h2.py
@@ -1,7 +1,7 @@
 import gdown
 
 # URL file Google Drive (thay đổi từ dạng "view" sang "uc" để gdown hoạt động)
-url = ""
+url = "https://api.cloud.hashicorp.com/vagrant-archivist/v1/object/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiIyMWZlYWNmYi0xMWY5LTRkMTEtOGM2OC0xMTQ5YmY1NmY2YzIiLCJtb2RlIjoiciIsImZpbGVuYW1lIjoid2luMTFtb2RyZHB3Zl8xLjBfcWVtdV9hbWQ2NC5ib3gifQ.WYMn2onERXAiIk9BHyZtMJZZirZS6H9tzJAC5Sj8KIA"
 
 # Đường dẫn lưu file sau khi tải
 output = "/mnt/a.qcow2"  # Đường dẫn đầy đủ nơi lưu file

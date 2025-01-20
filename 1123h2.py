047e20c4551ce619c2f40d3f66ffb6fcae74910b
Snhvn <snhvnnguyen@gmail.com>
2025-01-20 18:50:04 +0700

    Create 1123h2.py

---
+++ 1123h2.py
@@ -0,0 +1,12 @@
+import gdown
+
+# URL file Google Drive (thay đổi từ dạng "view" sang "uc" để gdown hoạt động)
+url = ""
+
+# Đường dẫn lưu file sau khi tải
+output = "/mnt/a.qcow2"  # Đường dẫn đầy đủ nơi lưu file
+
+# Tải file
+gdown.download(url, output, quiet=False)
+
+print(f"File đã được tải về và lưu tại: {output}")

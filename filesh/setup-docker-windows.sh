ca785478d2533f211d6b4d338744ba092a89506b
Snhvn <snhvnnguyen@gmail.com>
2025-02-07 22:24:55 +0700

    Update setup-docker-windows.sh

--- filesh/setup-docker-windows.sh
+++ filesh/setup-docker-windows.sh
@@ -1,6 +1,7 @@
+sudo chmod 666 /dev/kvm
 sudo apt install docker-compose -y
 sudo wget -O /isos/driver.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.240-1/virtio-win-0.1.240.iso &>/dev/null &
 while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
-wget -O compose.yaml https://raw.githubusercontent.com/Snhvn/Composefile/main/14-16/compose.yaml
+wget -O compose.yaml https://github.com/Snhvn/Composefile/raw/refs/heads/main/14-16/composecolab.yaml
 echo "thanh cong, ket noi bang port 5900 ở app VNC"
 sudo docker compose up

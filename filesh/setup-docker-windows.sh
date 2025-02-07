b0ab25a454b79c63ce24085a07bfed30f20c1701
Snhvn <snhvnnguyen@gmail.com>
2025-02-07 22:18:22 +0700

    Update setup-docker-windows.sh

--- filesh/setup-docker-windows.sh
+++ filesh/setup-docker-windows.sh
@@ -1,6 +1,4 @@
-sudo mkdir /isos/
-sudo wget -O /isos/a.iso "${{ github.event.inputs.link_iso }}" &>/dev/null &
-while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
+sudo apt install docker-compose -y
 sudo wget -O /isos/driver.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.240-1/virtio-win-0.1.240.iso &>/dev/null &
 while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
 wget -O compose.yaml https://raw.githubusercontent.com/Snhvn/Composefile/main/14-16/compose.yaml
